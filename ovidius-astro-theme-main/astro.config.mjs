8712cbf1295f7b3968335a0c654f4e382b4dcccb
Lee Sang Inn <f374800890@gmail.com>
2026-02-21 13:31:17 +0900

    ci: Add GitHub Actions workflow for Astro deployment

--- ovidius-astro-theme-main/astro.config.mjs
+++ ovidius-astro-theme-main/astro.config.mjs
@@ -7,7 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ovidius-astro-theme.netlify.app',
+    site: 'https://Merchantlee99.github.io',
+    base: '/sanginn-pm.github.io',
     vite: {
         plugins: [tailwindcss()]
     },
